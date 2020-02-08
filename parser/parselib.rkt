98ed05684d02e4c0e3013871994494055ff88f22
Kim Mackey, Jr <kim.mackey.jr@gmail.com>
2020-02-08 13:31:25 -0500

    Create parselib module
    Parselib can be (require ...)ed.
    
    Provides (parse txt) to parse text into syntax tree.
    
    Test racket file uses RackUnit.
    
    Can be run from command line or
    (require ...)ed to provide test suite.

---
+++ parser/parselib.rkt
@@ -0,0 +1,22 @@
+#!/usr/bin/env racket
+
+#lang racket
+
+(provide parse)
+
+(define (parse txt)
+  (let-values (
+    [(proc
+      out
+      in
+      err)
+     (subprocess #f
+                 #f
+                 (current-error-port)
+                 "parse.sh")])
+   (begin
+     (write-string txt in)
+     (close-output-port in)
+     (subprocess-wait proc)
+     (close-output-port in)
+     (cadr (read out)))))
---
+++ parser/test.rkt
@@ -0,0 +1,32 @@
+#!/usr/bin/env racket
+
+#lang racket
+
+(require rackunit
+         "parselib.rkt")
+
+(define/provide-test-suite
+  test-parselib
+  (check-equal? (parse "")
+                null
+                "empty parse")
+  (check-equal? (parse "var x;")
+                '((var x))
+                "declare x")
+  (check-equal? (parse (string-append
+                        "var x;"
+                        "var y = 3;"
+                        "var z = 1;"
+                        "x = y + z;"))
+                '((var x)
+                  (var y 3)
+                  (var z 1)
+                  (= x (+ y z)))
+                (string-append
+                 "declare x, y, and z, "
+                 "assign values, "
+                 "perform operations")))
+
+(module+ main
+  (require rackunit/text-ui)
+  (exit (run-tests test-parselib)))

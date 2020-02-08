1dd89b5c339dcbd4568809c3b5e32dbb988ce7ae
Kim Mackey, Jr <kim.mackey.jr@gmail.com>
2020-02-08 16:16:46 -0500

    Add tests to bool mapping for int->bool conversion
    Test that 0->false
              1->true

--- denotation/test/M-bool.rkt
+++ denotation/test/M-bool.rkt
@@ -17,7 +17,17 @@
    (check-equal?
      (mapping-value-value (M-bool 'true null))
      #t
-     "true maps to #t")))
+     "true maps to #t"))
+  (test-case
+   "conversion"
+   (check-equal?
+     (mapping-value-value (M-bool 0 null))
+     #f
+     "zero maps to #f")
+   (check-equal?
+     (mapping-value-value (M-bool 1 null))
+     #t
+     "one maps to #t")))
 
 (module+ main
   (require rackunit/text-ui)

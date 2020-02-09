686e14fc3000fa2ccb66c5313db48067e25a57a3
Kim Mackey, Jr <kim.mackey.jr@gmail.com>
2020-02-09 11:05:01 -0500

    Pretty parselib formatting

--- parser/parselib.rkt
+++ parser/parselib.rkt
@@ -5,18 +5,15 @@
 (provide parse)
 
 (define (parse txt)
-  (let-values (
-    [(proc
-      out
-      in
-      err)
-     (subprocess #f
-                 #f
-                 (current-error-port)
-                 "parse.sh")])
-   (begin
-     (write-string txt in)
-     (close-output-port in)
-     (subprocess-wait proc)
-     (close-output-port in)
-     (cadr (read out)))))
+  (let-values
+      ([(proc out in err)
+        (subprocess #f
+                    #f
+                    (current-error-port)
+                    "parse.sh")])
+    (begin
+      (write-string txt in)
+      (close-output-port in)
+      (subprocess-wait proc)
+      (close-output-port in)
+      (cadr (read out)))))

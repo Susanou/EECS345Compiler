abebf2a0012158dcb2ce405003a3782d8613480b
Kim Mackey, Jr <kim.mackey.jr@gmail.com>
2020-02-11 00:44:26 -0500

    Make interpreter structs for value or error
    Define structs for value return or error return from the
    interpret interface call.

--- interpreter/interpreter.rkt
+++ interpreter/interpreter.rkt
@@ -1,6 +1,12 @@
 #lang racket
 
-(provide interpret)
+(provide interpret
+         (struct-out interpreter-value)
+         (struct-out interpreter-error))
+
+(struct interpreter-value (value))
+
+(struct interpreter-error (message))
 
 (define (interpret filename)
   null)

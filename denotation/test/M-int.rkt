bf590d6e3843def7d0d54f22fc83c94dcc9cbc79
Kim Mackey, Jr <kim.mackey.jr@gmail.com>
2020-02-12 17:28:44 -0500

    Add test for 1 + 2 in M-int
    Hardcoded handling.
    
    Will be used in testing for other M-* functions.

--- denotation/test/M-int.rkt
+++ denotation/test/M-int.rkt
@@ -10,7 +10,10 @@
   test-M-int
   (test-equal? "zero"
                (M-int 0 null)
-               (mapping-value 0)))
+               (mapping-value 0))
+  (test-equal? "1 + 2"
+               (M-int '(+ 1 2) null)
+               (mapping-value 3)))
 
 (module+ main
   (require rackunit/text-ui)

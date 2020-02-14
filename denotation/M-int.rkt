ed9f38eb5f8aae867553125d011ec00efe1094bb
Kim Mackey, Jr <kim.mackey.jr@gmail.com>
2020-02-14 14:31:18 -0500

    Implement M-int mock for testing

--- denotation/M-int.rkt
+++ denotation/M-int.rkt
@@ -5,8 +5,11 @@
 (require "mapping.rkt")
 
 (define (M-int expression state)
-  (mapping-value 
-    (cond 
-    ((equal? expression '(+ 1 2)) 3)
-    ((equal? expression 1) 1)
-    (else 0))))
+  (mapping-value (hash-ref (hash
+                            '(+ 1 2)                          3
+                            '(- (/ (* 6 (+ 8 (% 5 3))) 11) 9) -4
+                            '(+ (* 6 (- (* 4 2))) 9)          -39)
+                           expression
+                           (if (integer? expression)
+                               expression
+                               0))))

3c06ca065c7e83793800bd7878ecfcce876e3573
Kim Mackey, Jr <kim.mackey.jr@gmail.com>
2020-02-14 14:17:41 -0500

    Add interpreter tests for provided examples
    Example source code in own file, manifest file describes
    expected output.

--- interpreter/examples-manifest.rkt
+++ interpreter/examples-manifest.rkt
@@ -11,8 +11,56 @@
 
 (define-runtime-path EXAMPLES-DIR "examples/")
 
+(define provided-example-value-tests
+  (map (lambda (p)
+         (example (format "provided example #~s" (first p))
+                  (build-path EXAMPLES-DIR
+                              "provided"
+                              (number->string (first p)))
+                  (interpreter-value (second p))))
+       '((1 150)
+         (2 -4)
+         (3 10)
+         (4 16)
+         (5 220)
+         (6 5)
+         (7 6)
+         (8 10)
+         (9 5)
+         (10 -39)
+         (15 true)
+         (16 100)
+         (17 false)
+         (18 true)
+         (19 128)
+         (20 12)
+         (21 30)
+         (22 11)
+         (23 1106)
+         (24 12)
+         (25 16)
+         (26 72)
+         (27 21)
+         (28 164))))
+
+(define provided-example-error-tests
+  (map (lambda (p)
+         (example (format "provided example [error] #~s" (first p))
+                  (build-path EXAMPLES-DIR
+                              "provided"
+                              (number->string (first p)))
+                  (interpreter-error (second p))))
+       '((11 "using before declare")
+         (12 "using before declare")
+         (13 "use before assign")
+         (14 "redefining"))))
+
 (define examples-manifest
-  (list (example "return zero"
-                 (build-path EXAMPLES-DIR
-                             "return-zero.txt")
-                 (interpreter-value 0))))
+  (append
+   (list
+    (example "return zero"
+             (build-path EXAMPLES-DIR
+                         "return-zero.txt")
+             (interpreter-value 0)))
+   provided-example-value-tests
+   provided-example-error-tests))

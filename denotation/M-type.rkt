01c4143e2e0efec4fae01dbc4a54e872de20e23e
Kim Mackey, Jr <kim.mackey.jr@gmail.com>
2020-02-15 14:56:56 -0500

    Implement M-type for literals and expressions
    Does not yet support mapped variable types.

--- denotation/M-type.rkt
+++ denotation/M-type.rkt
@@ -4,6 +4,43 @@
 
 (require "mapping.rkt")
 
+(define BOOL 'BOOL)
+(define INT  'INT)
+
+(define MAPPING-BOOL (mapping-value BOOL))
+(define MAPPING-INT  (mapping-value INT))
+
+(define boolean-literals
+  '(true false))
+
+(define (boolean-literal? expression)
+  (member expression boolean-literals))
+
+(define integer-literal?
+  integer?)
+
+(define boolean-operators
+  '(! || && == != <= >= < >))
+
+(define integer-operators
+  '(+ - * / %))
+
+(define (operator-expression-check operators)
+  (lambda (expression)
+    (and (pair? expression)
+         (member (car expression)
+                 operators))))
+
+(define boolean-operator?
+  (operator-expression-check boolean-operators))
+
+(define integer-operator?
+  (operator-expression-check integer-operators))
+
 (define (M-type expression state)
-  (mapping-value (cond [(equal? expression 'false) 'BOOL]
-                       [else                       'INT])))
+  (cond [(boolean-literal?  expression) MAPPING-BOOL]
+        [(boolean-operator? expression) MAPPING-BOOL]
+        [(integer-literal?  expression) MAPPING-INT]
+        [(integer-operator? expression) MAPPING-INT]
+        [else
+         (mapping-error "unsupported type")]))
--- denotation/test/M-type.rkt
+++ denotation/test/M-type.rkt
@@ -8,9 +8,30 @@
 
 (define/provide-test-suite 
   test-M-type
-  (test-equal? "zero"
-               (M-type 0 null)
-               (mapping-value 'INT)))
+  (test-suite
+   "integer literals"
+   (test-equal? "zero"
+                (M-type 0 null)
+                (mapping-value 'INT))
+   (test-equal? "one"
+                (M-type 1 null)
+                (mapping-value 'INT)))
+  (test-suite
+   "integer operations"
+   (test-equal? "+"
+                (M-type '(+ this that) null)
+                (mapping-value 'INT))
+  (test-suite
+   "boolean literals"
+   (test-equal? "false"
+                (M-type 'false null)
+                (mapping-value 'BOOL))
+   (test-equal? "true"
+                (M-type 'true null)
+                (mapping-value 'BOOL)))
+   (test-equal? "||"
+                (M-type '(|| this that) null)
+                (mapping-value 'BOOL))))
 
 (module+ main
   (require rackunit/text-ui)

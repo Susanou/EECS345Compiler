c37dfadb6305a8db5563ce1b03c23b3fdcb84e3b
Kim Mackey, Jr <kim.mackey.jr@gmail.com>
2020-02-17 14:51:43 -0500

    Create fresh M-int template
    Currently implements integer literals, variables, and the +

--- denotation/M-int.rkt
+++ denotation/M-int.rkt
@@ -6,15 +6,29 @@
          "../machine/binding.rkt"
          "../machine/machine-scope.rkt")
 
+(define (map-variable name state)
+  (if (machine-scope-bound? state name)
+      (let ([binding (machine-scope-ref state name)])
+        (if (eq? 'INT (binding-type binding))
+            (mapping-value (binding-value binding))
+            (mapping-error (format "variable not integer: ~s"
+                                   name))))
+      (mapping-error (format "use before bind: ~s"
+                             name))))
+
+(define operations
+  (hash '+ (lambda (args state)
+             (mapping-value
+              (+ (mapping-value-value (M-int (first  args) state))
+                 (mapping-value-value (M-int (second args) state)))))))
+
+(define (map-operation expression state)
+  (if (hash-has-key? operations (first expression))
+      ((hash-ref operations (first expression)) (rest expression) state)
+      (mapping-error "unrecognized operation")))
+
 (define (M-int expression state)
-  (mapping-value (hash-ref (hash
-                            '(+ 1 2)                          3
-                            '(- (/ (* 6 (+ 8 (% 5 3))) 11) 9) -4
-                            '(+ (* 6 (- (* 4 2))) 9)          -39
-                            '(/ (- (* 5 7) 3) 2)              16)
-                           expression
-                           (cond [(integer? expression) expression]
-                                 [(symbol?  expression)
-                                  (binding-value (machine-scope-ref state
-                                                                    expression))]
-                                 [else 0]))))
+  (cond [(integer? expression) (mapping-value expression)]
+        [(symbol?  expression) (map-variable  expression state)]
+        [(list?    expression) (map-operation expression state)]
+        [else                  (mapping-error "unrecognized expression")]))
--- denotation/test/M-int.rkt
+++ denotation/test/M-int.rkt
@@ -4,16 +4,44 @@
 
 (require rackunit
          "../mapping.rkt"
-         "../M-int.rkt")
+         "../M-int.rkt"
+         "../../machine/machine-scope.rkt"
+         "../../machine/machine.rkt"
+         "../../machine/binding.rkt")
 
 (define/provide-test-suite 
   test-M-int
-  (test-equal? "zero"
-               (M-int 0 null)
-               (mapping-value 0))
-  (test-equal? "1 + 2"
-               (M-int '(+ 1 2) null)
-               (mapping-value 3)))
+  (test-suite
+   "integers"
+   (test-equal? "zero"
+                (M-int 0 null)
+                (mapping-value 0))
+   (test-equal? "fize"
+                (M-int 5 null)
+                (mapping-value 5))
+   (test-equal? "negative three"
+                (M-int -3 null)
+                (mapping-value -3)))
+  (test-suite
+   "variables"
+   (test-equal? "ok"
+                (M-int 'x (machine-scope-bind (machine-new)
+                                              'x
+                                              (binding 'INT 3)))
+                (mapping-value 3))
+   (test-equal? "wrong type"
+                (M-int 'x (machine-scope-bind (machine-new)
+                                              'x
+                                              (binding 'NULL null)))
+                (mapping-error "variable not integer: x"))
+   (test-equal? "unbound"
+                (M-int 'x (machine-new))
+                (mapping-error "use before bind: x")))
+  (test-suite
+   "expressions"
+   (test-equal? "plus"
+                (M-int '(+ 1 2) null)
+                (mapping-value 3))))
 
 (module+ main
   (require rackunit/text-ui)

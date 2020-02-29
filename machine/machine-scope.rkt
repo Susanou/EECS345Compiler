9952142f2680e0fdfbc46262b5dbd20c3e884cee
Kim Mackey, Jr <kim.mackey.jr@gmail.com>
2020-02-29 16:14:14 -0500

    BREAK implement nested machine scopes
    Converted machine tests, but no other usage, so breaks.

--- machine/machine-scope.rkt
+++ machine/machine-scope.rkt
@@ -1,24 +1,75 @@
 #lang racket
 
-(provide machine-scope-bound?
-         machine-scope-bind
-         machine-scope-ref)
+(provide machine-scope-push
+         machine-scope-pop
+         machine-scope-has?
+         machine-bound-top?
+         machine-bound-any?
+         machine-bind-new
+         machine-bind-current
+         machine-ref)
 
 (require "machine.rkt")
 
-(define (machine-scope-bound? state name)
-  (hash-has-key? (machine-scope state)
-                 name))
-
-(define (machine-scope-bind state name binding)
+(define (machine-scope-push state)
   (struct-copy machine
                state
-               (scope
-                (hash-set
-                 (machine-scope state)
-                 name
-                 binding))))
+               (scopes (cons (hash)
+                             (machine-scopes state)))))
+  
+(define (machine-scope-pop state)
+  (struct-copy machine
+               state
+               (scopes (rest (machine-scopes state)))))
 
-(define (machine-scope-ref state name)
-  (hash-ref (machine-scope state)
-            name))
+(define (machine-scope-has? state)
+  (not (null? (machine-scopes state))))
+
+(define (scope-bound? scope name)
+  (hash-has-key? scope name))
+
+(define (scope-bound-any? scopes name)
+  (and (pair? scopes)
+       (or (scope-bound?     (first scopes) name)
+           (scope-bound-any? (rest  scopes) name))))
+
+(define (machine-bound-top? state name)
+  (scope-bound? (first (machine-scopes state)) name))
+
+(define (machine-bound-any? state name)
+  (scope-bound-any? (machine-scopes state) name))
+
+(define (scope-bind-new scope name value)
+  (cons (hash-set (first scope) name value)
+        (rest scope)))
+
+(define (scope-bind-current scope name value)
+  (if (scope-bound?   (first scope) name)
+      (cons (hash-set (first scope) name value)
+            (rest scope))
+      (cons           (first scope)
+                      (scope-bind-current (rest scope)
+                                          name
+                                          value))))
+
+(define (machine-bind-new state name value)
+  (struct-copy machine
+               state
+               (scopes (scope-bind-new (machine-scopes state)
+                                       name
+                                       value))))
+
+(define (machine-bind-current state name value)
+  (struct-copy machine
+               state
+               (scopes (scope-bind-current (machine-scopes state)
+                                           name
+                                           value))))
+
+(define (scope-ref scope name)
+  (if (scope-bound? (first scope) name)
+      (hash-ref     (first scope) name)
+      (scope-ref    (rest scope)  name)))
+
+(define (machine-ref state name)
+  (scope-ref (machine-scopes state) name))
--- machine/test/machine-scope.rkt
+++ machine/test/machine-scope.rkt
@@ -6,39 +6,41 @@
          "../machine.rkt"
          "../machine-scope.rkt")
 
+(define (test-state name state var top any value)
+  (test-suite
+   name
+   (test-equal? "bound top"
+                (machine-bound-top? state var)
+                top)
+   (test-equal? "bound any"
+                (machine-bound-any? state var)
+                any)
+   (if (not (null? value))
+       (test-equal? "bound value"
+                    (machine-ref    state var)
+                    value)
+       (void))))
+
 (define/provide-test-suite
   test-machine-scope
-  (let* ([empty-machine (machine-new)]
-         [a-machine
-          (machine-scope-bind empty-machine
-                              'name
-                              'a-value)]
-         [b-machine
-          (machine-scope-bind a-machine
-                              'name
-                              'b-value)])
-    (test-false "empty machine does not have name in scope"
-                (machine-scope-bound? empty-machine
-                                      'name))
-    (test-true "machine-A does have name in scope"
-               (machine-scope-bound? a-machine
-                                     'name))
-    (test-equal? "machine-A has correct value bound to name"
-                 (machine-scope-ref a-machine 'name)
-                 'a-value)
-    (test-false "machine-A does not have not-name in scope"
-                (machine-scope-bound? a-machine
-                                      'not-name))
-    (test-true "machine-B does have name in scope"
-               (machine-scope-bound? b-machine
-                                     'name))
-    (test-equal? "machine-B has correct value bound to name"
-                 (machine-scope-ref b-machine 'name)
-                 'b-value)
-    (test-false "machine-B does not have not-name in scope"
-                (machine-scope-bound? b-machine
-                                      'not-name))))
-
+  (let* ([a (machine-new                )]
+         [b (machine-bind-new     a 'x 1)]
+         [c (machine-bind-current b 'x 2)]
+         [d (machine-scope-push   c     )]
+         [e (machine-bind-current d 'x 3)]
+         [f (machine-bind-new     e 'x 4)]
+         [g (machine-bind-current f 'x 5)]
+         [h (machine-scope-pop    g     )])
+    (test-suite
+     "state"
+     (test-state "a" a 'x #f #f null)
+     (test-state "b" b 'x #t #t 1)
+     (test-state "c" c 'x #t #t 2)
+     (test-state "d" d 'x #f #t 2)
+     (test-state "e" e 'x #f #t 3)
+     (test-state "f" f 'x #t #t 4)
+     (test-state "g" g 'x #t #t 5)
+     (test-state "h" h 'x #t #t 3))))
 
 (module+ main
   (require rackunit/text-ui)

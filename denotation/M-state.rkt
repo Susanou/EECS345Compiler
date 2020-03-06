d75b8fdbddcfa05f92a49fb4b86f501e118144a1
Kim Mackey, Jr <kim.mackey.jr@gmail.com>
2020-03-05 19:15:23 -0500

    Implement continue

--- denotation/M-state.rkt
+++ denotation/M-state.rkt
@@ -13,90 +13,117 @@
 
 (define operations
   (hash
-   RETURN  (lambda (args state return)
-             (try (M-value (single-argument args) state)
-                  (lambda (value)
-                    (return value state))))
+   RETURN   (lambda (args state return continue)
+              (try (M-value (single-argument args) state)
+                   (lambda (value)
+                     (return value state))))
 
-   DECLARE (lambda (args state return)
-             (let ([name (left-argument  args)])
-               (if (machine-bound-top? state name)
-                   (failure (format "redefining: ~a" name))
-                   (if (binary-argument? args)
-                       (try (M-value (right-argument args) state)
-                            (lambda (init)
-                              (success (machine-bind-new state
-                                                         name
-                                                         init))))
-                       (success (machine-bind-new state
-                                                  name
-                                                  null))))))
+   DECLARE  (lambda (args state return continue)
+              (let ([name (left-argument  args)])
+                (if (machine-bound-top? state name)
+                    (failure (format "redefining: ~a" name))
+                    (if (binary-argument? args)
+                        (try (M-value (right-argument args) state)
+                             (lambda (init)
+                               (success (machine-bind-new state
+                                                          name
+                                                          init))))
+                        (success (machine-bind-new state
+                                                   name
+                                                   null))))))
 
-   ASSIGN  (lambda (args state return)
-             (let ([name  (left-argument  args)])
-               (if (machine-bound-any? state name)
-                   (try (M-value (right-argument args) state)
-                        (lambda (value)
-                          (success (machine-bind-current state
-                                                         name
-                                                         value))))
-                   (failure (format "assign before declare: ~s"
-                                    name)))))
+   ASSIGN   (lambda (args state return continue)
+              (let ([name  (left-argument  args)])
+                (if (machine-bound-any? state name)
+                    (try (M-value (right-argument args) state)
+                         (lambda (value)
+                           (success (machine-bind-current state
+                                                          name
+                                                          value))))
+                    (failure (format "assign before declare: ~s"
+                                     name)))))
 
-   IF      (lambda (args state return)
-             (try (M-bool (first-argument args) state)
-                  (lambda (condition)
-                    (if condition
-                        (M-state (second-argument args)
-                                 state
-                                 return)
-                        (if (triady-argument? args)
-                            (M-state (third-argument args)
-                                     state
-                                     return)
-                            (success state))))))
-
-   WHILE   (lambda (args state return)
-             (try (M-bool (left-argument args) state)
-                  (lambda (condition)
-                    (if condition
-                        (try (M-state (right-argument args)
+   IF       (lambda (args state return continue)
+              (try (M-bool (first-argument args) state)
+                   (lambda (condition)
+                     (if condition
+                         (M-state (second-argument args)
+                                  state
+                                  return
+                                  continue)
+                         (if (triady-argument? args)
+                             (M-state (third-argument args)
                                       state
-                                      return)
-                             (lambda (state)
-                               (M-state (single-expression WHILE args)
-                                        state
-                                        return)))
-                        (success state)))))
+                                      return
+                                      continue)
+                             (success state))))))
 
-   BLOCK   (lambda (args state return)
-             (if (null? args)
-                 (success state)
-                 (try (M-state (first args) state return)
-                      (lambda (state)
-                        (M-state (single-expression BLOCK (rest args))
-                                 state
-                                 return)))))
+   WHILE    (lambda (args state return continue)
+              (try (M-bool (left-argument args) state)
+                   (lambda (condition)
+                     (if condition
+                         (try (let/cc c
+                                (M-state (right-argument args)
+                                         state
+                                         return
+                                         (lambda (state)
+                                           (c (success state)))))
+                              (lambda (state)
+                                (M-state (single-expression WHILE args)
+                                         state
+                                         return
+                                         continue)))
+                         (success state)))))
 
-   BEGIN   (lambda (args state return)
-             (try (M-state (single-expression BLOCK args)
-                           (machine-scope-push state)
-                           (lambda (value state)
-                             (return value
-                                     (machine-scope-pop state))))
-                  (lambda (state)
-                    (success (machine-scope-pop state)))))))
+   CONTINUE (lambda (args state return continue)
+              (continue state))
 
-(define (operation exp state return)
+   BLOCK    (lambda (args state return continue)
+              (if (null? args)
+                  (success state)
+                  (try (M-state (first args)
+                                state
+                                return
+                                continue)
+                       (lambda (state)
+                         (M-state (single-expression BLOCK (rest args))
+                                  state
+                                  return
+                                  continue)))))
+
+   BEGIN    (lambda (args state return continue)
+              (try (M-state (single-expression BLOCK args)
+                            (machine-scope-push state)
+                            (lambda (value state)
+                              (return value
+                                      (machine-scope-pop state)))
+                            continue)
+                   (lambda (state)
+                     (success (machine-scope-pop state)))))))
+
+(define (operation exp state return continue)
   (let ([op (operator exp)])
     (if (hash-has-key? operations op)
-        ((hash-ref     operations op) (arguments exp) state return)
+        ((hash-ref     operations op) (arguments exp)
+                                      state
+                                      return
+                                      continue)
         (failure "unrecognized operation"))))
 
 (define no-return
   (thunk* (failure "unexpected return")))
 
-(define (M-state exp state (return no-return))
+(define no-continue
+  (thunk* (failure "continue outside loop")))
+
+(define (M-state
+         exp
+         state
+         (return no-return)
+         (continue no-continue))
   (if (EXPRESSION? exp)
-      (operation exp state return)
+      (operation exp
+                 state
+                 return
+                 continue)
       (success state)))
--- denotation/test/continue.rkt
+++ denotation/test/continue.rkt
@@ -3,12 +3,38 @@
 #lang racket
 
 (require rackunit
-         "../M-state.rkt"
+         "../../functional/either.rkt"
          "../../machine/machine-scope.rkt"
-         "../../machine/machine.rkt")
+         "../../machine/machine.rkt"
+         "../M-state.rkt")
 
 (define/provide-test-suite 
-  test-continue)
+  test-continue
+  (test-equal? "error result"
+               (M-state '(continue)
+                        (machine-new))
+               (failure "continue outside loop"))
+  (let ([mstate (M-state '(block
+                           (var x 0)
+                           (var y 0)
+                           (while (< x 5)
+                                  (begin
+                                    (= x (+ x 1))
+                                    (if (> x 2)
+                                        (continue))
+                                    (= y (+ y 1)))))
+                         (machine-new))])
+    (test-pred "successful result"
+               success?
+               mstate)
+    (on mstate
+        (lambda (state)
+          (test-equal? "loop ran 5 times"
+                       (machine-ref state 'x)
+                       5)
+          (test-equal? "loop continued thrice"
+                       (machine-ref state 'y)
+                       2)))))
   
 (module+ main
   (require rackunit/text-ui)

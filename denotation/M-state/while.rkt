3925c022bbdc53804dd46903b9ec1cf2277df93c
Kim Mackey, Jr <kim.mackey.jr@gmail.com>
2020-03-12 22:23:52 -0400

    Implement WHILE side effects

--- denotation/M-state/while.rkt
+++ denotation/M-state/while.rkt
@@ -12,18 +12,21 @@
                        state
                        return
                        continue)
-  (try (M-bool (left-argument args) state M-state)
-       (lambda (condition)
-         (if condition
-             (try (let/cc c
-                    (M-state (right-argument args)
-                             state
-                             return
-                             (lambda (state)
-                               (c (success state)))))
-                  (lambda (state)
-                    (M-state (single-expression WHILE args)
-                             state
-                             return
-                             continue)))
-             (success state)))))
+  (let ([condition-arg (left-argument args)])
+    (try (M-bool condition-arg state M-state)
+         (lambda (condition)
+           (try (M-state condition-arg state)
+                (lambda (state)
+                  (if condition
+                      (try (let/cc c
+                             (M-state (right-argument args)
+                                      state
+                                      return
+                                      (lambda (state)
+                                        (c (success state)))))
+                           (lambda (state)
+                             (M-state (single-expression WHILE args)
+                                      state
+                                      return
+                                      continue)))
+                      (success state))))))))

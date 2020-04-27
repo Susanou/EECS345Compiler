4b4af92a81efb81e6234d87493406fd905459efa
Daedagor <48215679+Daedagor@users.noreply.github.com>
2020-04-27 18:41:37 -0400

    Update closure.rkt
    Created and provided function closure

--- denotation/closure.rkt
+++ denotation/closure.rkt
@@ -1,6 +1,10 @@
 #lang racket
 
 (provide (struct-out closure))
+(provide (struct-out fclosure))
 
-(struct closure (parameters body level functclass)
+(struct closure (parameters body level)
   #:transparent)
+  
+(struct fclosure (parameters body level functclass)
+   #:transparent)
